78b7283eef3b1eb9dd62bd41f116a95d1ed4a592
aaronrood <aaron.s.rood@gmail.com>
2022-04-07 17:41:05 -0400

    find ISS and update map with its location

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 // Initialize and add the map
 function initMap() {
-  setInterval(fetchMap, 10000);
+  setInterval(fetchMap, 5000);
 }
 
 function fetchMap( )
 {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
-  // The map, centered at Uluru
+  httpGetAsync('http://api.open-notify.org/iss-now.json', updateMap);
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function updateMap(res) {
+  const resParsed = JSON.parse(res);
+  const lat = resParsed.iss_position.latitude;
+  const long = resParsed.iss_position.longitude;
+
+  const iss = { lat: lat, lng: long };
+  // The map, centered at iss location
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
-    center: uluru,
+    center: iss,
   });
-  // The marker, positioned at Uluru
+  // The marker, positioned at iss location
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: iss,
     map: map,
   });
 }
